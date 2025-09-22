f30feb8da9699c3c7fa2da0687f6a440fd1f8bfe
banbezv <banbezv@mail.ru>
2025-09-22 11:30:18 +0300

    Map added

--- src/main/java/pro/sky/employeeservice/service/EmployeeService.java
+++ src/main/java/pro/sky/employeeservice/service/EmployeeService.java
@@ -5,52 +5,54 @@ import pro.sky.employeeservice.exception.EmployeeAlreadyAddedException;
 import pro.sky.employeeservice.exception.EmployeeNotFoundException;
 import pro.sky.employeeservice.exception.EmployeeStorageIsFullException;
 import pro.sky.employeeservice.model.Employee;
+import pro.sky.employeeservice.model.EmployeeId;
 
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Collection;
 import java.util.List;
 
 @Service
 public class EmployeeService {
-    private List<Employee> employees;
+    private Map<EmployeeId, Employee> employees;
     private int maxSize;
 
     public EmployeeService() {
-        employees = new ArrayList<Employee>();
+        employees = new HashMap<EmployeeId, Employee>();
         maxSize = 3;
     }
 
     public Collection<Employee> getAll() {
-        return List.copyOf(employees);
+        return List.copyOf(employees.values());
     }
 
     public Employee addEmployee(String name, String surname) {
         if (employees.size() >= maxSize) {
             throw new EmployeeStorageIsFullException();
         }
+        EmployeeId employeeId = new EmployeeId(name, surname);
         Employee employee = new Employee(name, surname);
-        if (employees.contains(employee)) {
+        if (employees.containsKey(employeeId)) {
             throw new EmployeeAlreadyAddedException();
         }
-        employees.add(employee);
-        return employees.get(employees.indexOf(employee));
+        employees.put(employeeId, employee);
+        return employees.get(employeeId);
     }
 
     public Employee findEmployee(String name, String surname) {
-        Employee employee = new Employee(name, surname);
-        if (!employees.contains(employee)) {
+        EmployeeId employeeId = new EmployeeId(name, surname);
+        if (!employees.containsKey(employeeId)) {
             throw new EmployeeNotFoundException();
         }
-        return employees.get(employees.indexOf(employee));
+        return employees.get(employeeId);
     }
 
     public Employee removeEmployee(String name, String surname) {
-        Employee employee = new Employee(name, surname);
-        if (!employees.contains(employee)) {
+        EmployeeId employeeId = new EmployeeId(name, surname);
+        if (!employees.containsKey(employeeId)) {
             throw new EmployeeNotFoundException();
         }
-        Employee removedEmployee = employees.get(employees.indexOf(employee));
-        employees.remove(employee);
+        Employee removedEmployee = employees.remove(employeeId);
         return removedEmployee;
     }
 }
